a810a0c54ae67860a48aab3b6a80779576ee0778
Jchub58 <jayavelchellapandi@gmail.com>
2025-12-07 20:16:33 +0530

    Enhanced BasePage with additional utility methods

--- src/main/java/com/saucedemo/pages/BasePage.java
+++ src/main/java/com/saucedemo/pages/BasePage.java
@@ -1,11 +1,13 @@
 package com.saucedemo.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
 
 public class BasePage {
     protected WebDriver driver;
@@ -30,5 +32,41 @@ public class BasePage {
 
     protected String waitAndGetText(By locator) {
         return waitForElement(locator).getText();
+    }
+
+    protected boolean isElementVisible(By locator) {
+        try {
+            return waitForElement(locator).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    protected void clearAndType(By locator, String text) {
+        WebElement element = waitForElement(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected void waitForElementToDisappear(By locator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected List<WebElement> waitForElements(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+    }
+
+    protected void scrollToElement(By locator) {
+        WebElement element = waitForElement(locator);
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView(true);", element);
+    }
+
+    protected String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    protected String getPageTitle() {
+        return driver.getTitle();
     }
 }
