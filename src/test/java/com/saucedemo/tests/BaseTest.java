e2d46c46e49bb4e09a644960dd0371457b44d4ab
jchub58 <jayavelchellapandi@gmail.com>
2025-12-08 12:20:36 +0530

    resolve package issues (#2)
    * Add DriverFactory
    
    * Update BaseTest.java - Used driverfactory
    
    * fix package
    
    * import fix

--- src/test/java/com/saucedemo/tests/BaseTest.java
+++ src/test/java/com/saucedemo/tests/BaseTest.java
@@ -1,26 +1,22 @@
 package com.saucedemo.tests;
 
-import com.saucedemo.utils.ConfigReader;
+import com.saucedemo.utils.DriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
+import com.saucedemo.utils.ConfigReader;
 
 public class BaseTest {
     protected WebDriver driver;
-
+    
     @BeforeMethod
     public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver = DriverFactory.initDriver();
         driver.get(ConfigReader.getProperty("base.url"));
     }
-
+    
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        DriverFactory.quitDriver();
     }
 }
